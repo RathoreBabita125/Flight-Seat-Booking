2444329d975595b5be25026a54ebdeb632b39691
Babita Dulichand <Babita>
2026-08-07 23:56:51 +0530

    fix: add production start command for render

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -2,13 +2,17 @@ import { DataSource } from "typeorm";
 import { User } from "../modules/userModule";
 import { Booking } from "../modules/bookingModule";
 import { Seat } from "../modules/seatModule";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const AppDataSource=new DataSource({
     type:'postgres',
-    port:5432,
-    username:'postgres',
-    password:"Cel%Bd@2026",
-    database:'Flight Seat Booking',
+    url:process.env.DATABASE_URL,
+    // port:5432,
+    // username:'postgres',
+    // password:"Cel%Bd@2026",
+    // database:'Flight Seat Booking',
     synchronize:true,
     entities:[User, Booking, Seat]
+
 })
