de0d5d757c73ae2faa32fffe32adfc18e975c58b
Babita Dulichand <Babita>
2026-08-08 00:34:54 +0530

    update app.ts file

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -3,12 +3,16 @@ import { User } from "../modules/userModule";
 import { Booking } from "../modules/bookingModule";
 import { Seat } from "../modules/seatModule";
 
-export const AppDataSource=new DataSource({
-    type:'postgres',
-    port:5432,
-    username:'postgres',
-    password:"Cel%Bd@2026",
-    database:'Flight Seat Booking',
-    synchronize:true,
-    entities:[User, Booking, Seat]
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    // port: 5432,
+    // username: 'postgres',
+    // password: "Cel%Bd@2026",
+    // database: 'Flight Seat Booking',
+    synchronize: true,
+    entities: [User, Booking, Seat]
 })
